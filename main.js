50ba537060f411ffa989a4adf227944d2c71be7f
Moderrek <tymonek12345@gmail.com>
2024-06-09 09:35:46 +0200

    Improved code and added clock marks

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ const play = () => {
  * Gets the center of the canvas
  * @returns {{x: number, y: number}}
  */
-const center = () => {
+const getCenter = () => {
   return { x: canvas.width / 2, y: canvas.height / 2 };
 };
 
@@ -36,89 +36,111 @@ const draw = () => {
   ctx.textAlign = "center";
   ctx.textBaseline = "middle"
 
-  const d = new Date();
+  const now = new Date();
 
   // If the seconds are the same, don't draw
-  if (lastDate && lastDate.getSeconds() === d.getSeconds()) {
+  if (lastDate && lastDate.getSeconds() === now.getSeconds()) {
     return;
   }
-  lastDate = d;
+  lastDate = now;
 
   // Clear the canvas
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-
-  const c = center();
-
+  
+  const center = getCenter();
+  // Draw marks
+  drawMarks();
   // Draw circle at center
-  ctx.beginPath();
-  ctx.arc(c.x, c.y, relative(5), 0, Math.PI * 2);
-  ctx.fill();
-
-  const r = relative(-5 + 1);
-
+  drawCircle(center, 5);
+  const centerOffset = -4;
   // Draw second hand
-  const sf = 12;
-  const sfs = 4;
-  const sl = 10;
-  ctx.font = `${relative(sf)}px Arial Light`;
-  const s = d.getSeconds();
-  const sp = d.getSeconds() / 60;
-  const sa = Math.PI * 2 * sp - Math.PI / 2;
-  const sacosine = Math.cos(sa);
-  const sasine = Math.sin(sa);
-  for (let start = { x: c.x + sacosine * r, y: c.y + sasine * r }, i = 0; i < sl; i++) {
-    start.x += sacosine * relative(sf + sfs);
-    start.y += sasine * relative(sf + sfs);
-    ctx.fillText(s, start.x, start.y);
-  }
-
+  drawHand(centerOffset, 12, "Arial Light", now.getSeconds(), now.getSeconds() / 60, 10, 4);
   // Draw minute hand
-  const mf = 18;
-  const mfs = 4;
-  const ml = 7;
-  ctx.font = `${relative(mf)}px Arial`;
-  const m = d.getMinutes();
-  const mp = m / 60;
-  const ma = Math.PI * 2 * mp - Math.PI / 2;
-  const macosine = Math.cos(ma);
-  const masine = Math.sin(ma);
-  for (let s = { x: c.x + macosine * r, y: c.y + masine * r }, i = 0; i < ml; i++) {
-    s.x += macosine * relative(mf + mfs);
-    s.y += masine * relative(mf + mfs);
-    ctx.fillText(m, s.x, s.y);
-  }
-
+  drawHand(centerOffset, 18, "Arial", now.getMinutes(), now.getMinutes() / 60, 7, 4);
   // Draw hour hand
-  const hf = 18;
-  const hfs = 0;
-  const hl = 5;
-  ctx.font = `${relative(hf)}px Arial Black`;
-  const h = d.getHours();
-  const hp = (h % 12) / 12;
-  const ha = Math.PI * 2 * hp - Math.PI / 2;
-  const hacosine = Math.cos(ha);
-  const hasine = Math.sin(ha);
-  for (let s = { x: c.x + hacosine * r, y: c.y + hasine * r }, i = 0; i < hl; i++) {
-    s.x += hacosine * relative(hf + hfs);
-    s.y += hasine * relative(hf + hfs);
-    ctx.fillText(h, s.x, s.y);
-  }
+  drawHand(centerOffset, 18, "Arial Black", now.getHours(), now.getHours() % 12 / 12, 5, 0);
 };
+
+const drawMarks = () => {
+  const center = getCenter();
+  const radius = relative(200);
+  for (let i = 0; i < 60; i++) {
+    const isHour = i % 5 == 0;
+    const width = isHour ? 3 : 2;
+    const length = relative(isHour ? 170 : 190);
+
+    const angle = Math.PI * 2 * i / 60 - Math.PI / 2;
+    const angleCosine = Math.cos(angle);
+    const angleSine = Math.sin(angle);
+
+    const start = {
+      x: center.x + angleCosine * radius,
+      y: center.y + angleSine * radius
+    };
+
+    const end = {
+      x: center.x + angleCosine * length,
+      y: center.y + angleSine * length
+    };
+
+    ctx.lineWidth = relative(width);
+    drawLine(start, end);
+  }
+}
+
+const drawLine = (start, end) => {
+  ctx.beginPath();
+  ctx.moveTo(start.x, start.y);
+  ctx.lineTo(end.x, end.y);
+  ctx.stroke();
+}
+
+const drawCircle = (center, radius) => {
+  ctx.beginPath();
+  ctx.arc(center.x, center.y, relative(radius), 0, Math.PI * 2);
+  ctx.fill();
+}
+
+const drawHand = (fromCenterDistance, fontSize, font, value, percentage, sequenceLength, spacing) => {
+  // Set the font
+  ctx.font = `${relative(fontSize)}px ${font}`;
+  // Calculate the angle
+  const angleInRadians = Math.PI * 2 * percentage - Math.PI / 2;
+  // Calculate the cosine and sine of the angle
+  const angleCosine = Math.cos(angleInRadians);
+  const angleSine = Math.sin(angleInRadians);
+  // Calculate the start position
+  const center = getCenter();
+  const radiusFromCenter = relative(fromCenterDistance);
+  let start = { 
+    x: center.x + angleCosine * radiusFromCenter,
+    y: center.y + angleSine * radiusFromCenter
+  };
+  // Draw the text and calculate the next positions
+  for (i = 0; i < sequenceLength; i++) {
+    start.x += angleCosine * relative(fontSize + spacing);
+    start.y += angleSine * relative(fontSize + spacing);
+    ctx.fillText(value, start.x, start.y);
+  }
+}
 
 // Invoked when the DOM is fully loaded
 const onLoad = () => {
   canvas = document.querySelector("canvas");
-  onResize();
-  play();
+  onResize(); // Set the initial size
+  play(); // Start the game loop
 };
 
 const onResize = () => {
   if (!canvas) return;
+  
   const { innerWidth, innerHeight } = window;
   const size = Math.min(innerWidth, innerHeight);
-  canvas.width = size * 0.9;
-  canvas.height  = size * 0.9;
+  const scale = 0.9;
+  
+  canvas.width = size * scale;
+  canvas.height  = size * scale;
+
   lastDate = null; // Force redraw
 }
 
